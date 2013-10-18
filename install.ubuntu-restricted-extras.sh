6c43a576e0524a41a50d2e016ec956093ab5698d
panticz <mail@konczalski.de>
2013-10-18 11:04:15 +0200

    Remove not used Java license agreement

--- install.ubuntu-restricted-extras.sh
+++ install.ubuntu-restricted-extras.sh
@@ -4,7 +4,6 @@ sudo apt-get install -y debconf-utils
 
 # create answer file
 sudo debconf-set-selections <<\EOF
-sun-java6-bin shared/accepted-sun-dlj-v1-1 boolean true
 msttcorefonts msttcorefonts/defoma note
 ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula boolean true
 ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note
