275ddb5eb9764698d19d8d72a8c45aba6364ffec
panticz <mail@konczalski.de>
2013-10-21 12:28:15 +0200

    Create install.hibiscus.sh

---
+++ install.hibiscus.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# install Java
+wget http://installit.googlecode.com/hg/install.java-jdk.sh -O - | bash -
+
+# download Jameica
+if [ "$(uname -m)" == "x86_64" ]; then
+  # 64 bit
+  URL=http://www.willuhn.de/products/jameica/releases/current/jameica/jameica-linux.zip
+else
+  # 32 bit
+  URL=http://www.willuhn.de/products/jameica/releases/current/jameica/jameica-linux64.zip
+fi
+wget ${URL} -P /tmp
+
+# install Jameica
+sudo unzip /tmp/jameica-linux*.zip -d /opt/
+
+# download Hibiscus
+wget http://www.willuhn.de/products/hibiscus/releases/current/hibiscus.zip -P /tmp
+
+# install Hibiscus
+sudo unzip /tmp/hibiscus.zip -d /opt/jameica/plugins/
+
+# create start entry
+cat << EOF | sudo tee -a "/usr/share/applications/Hibiscus.desktop"
+[Desktop Entry]
+Version=1.0
+Encoding=UTF-8
+Name=Hibiscus
+Type=Application
+Terminal=false
+Exec=/opt/jameica/jameica.sh
+Icon=/opt/jameica/jameica-icon.png
+Categories=Office;Finance;
+EOF
