5de5ab8148af09dc9d78b69839890355a6217338
panticz <mail@konczalski.de>
2013-10-21 15:35:20 +0200

    Create install.skype.sh

---
+++ install.skype.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# add partner repository
+sudo add-apt-repository -y "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+
+# update
+sudo apt-get update
+
+# install
+sudo apt-get install -y skype
