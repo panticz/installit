e33db1af0295ee0b3d64a50102d0fcd46c01a48a
panticz <mail@konczalski.de>
2013-10-22 14:34:14 +0200

    use quiet download

--- install.netbeans.sh
+++ install.netbeans.sh
@@ -4,10 +4,10 @@
 URL=http://download.netbeans.org/netbeans/7.4/final/bundles/netbeans-7.4-javase-linux.sh
 
 # install Java JDK
-wget https://raw.github.com/panticz/installit/master/install.java-jdk.sh -O - | bash -
+wget -q https://raw.github.com/panticz/installit/master/install.java-jdk.sh -O - | bash -
 
 # download and install netbeans from homepage
-wget ${URL} -O /tmp/netbeans-linux.sh
+wget -q ${URL} -O /tmp/netbeans-linux.sh
 sudo bash /tmp/netbeans-linux.sh --silent
 
 # cleanup
