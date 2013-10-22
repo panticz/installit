32ca212c2523567413097ce98240f20eb7ca3387
panticz <mail@konczalski.de>
2013-10-22 11:45:20 +0200

    Fix tabulator

--- install.teamviewer.sh
+++ install.teamviewer.sh
@@ -4,11 +4,11 @@
 sudo apt-get install -y libc6-i386 lib32asound2 lib32z1 ia32-libs
 
 if [ "$(uname -m)" == "x86_64" ]; then
-﻿  # 64 bit
-﻿  URL=http://www.teamviewer.com/download/version_8x/teamviewer_linux_x64.deb
+  # 64 bit
+  URL=http://www.teamviewer.com/download/version_8x/teamviewer_linux_x64.deb
 else
-﻿  # 32 bit
-﻿  URL=http://www.teamviewer.com/download/version_8x/teamviewer_linux.deb
+  # 32 bit
+  URL=http://www.teamviewer.com/download/version_8x/teamviewer_linux.deb
 fi
 
 # download
