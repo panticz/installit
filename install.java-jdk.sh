5cfe04b0a77e10fd56fb225a3169a9dc474a56b1
panticz <mail@konczalski.de>
2013-10-22 15:27:18 +0200

    Workaround to disable APT proxy

--- install.java-jdk.sh
+++ install.java-jdk.sh
@@ -14,8 +14,14 @@ sudo add-apt-repository -y ppa:webupd8team/java
 # update repository
 sudo apt-get update -qq
 
+# WORKAROUND: disable apt proyx
+sudo sed -i 's|Acquire::http::Proxy|#Acquire::http::Proxy|g' /etc/apt/apt.conf
+
 # install
 sudo apt-get -o Acquire::http:Proxy=DIRECT install -y oracle-java7-installer
+
+# WORKAROUND: enable apt proxy
+sudo sed -i 's|#Acquire::http::Proxy|Acquire::http::Proxy|g' /etc/apt/apt.conf
 
 # set java 7 as default java version
 sudo update-java-alternatives -s java-7-oracle
