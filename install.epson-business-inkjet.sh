dcae42485c95459078e0e54d7b3fb3c301afb41a
panticz <mail@konczalski.de>
2013-10-23 14:26:58 +0200

    Fix comments

--- install.epson-business-inkjet.sh
+++ install.epson-business-inkjet.sh
@@ -6,7 +6,7 @@ sudo sed -i "s/# \(deb .* universe\)/\1/" /etc/apt/sources.list
 # install libs
 sudo apt-get install -y libxml2
 
-# get b500 driver
+# download driver package
 if [ ! -f /tmp/pips-pxb500-Ubuntu10.04-3.2.0-CG.tgz ]; then
    wget http://a1227.g.akamai.net/f/1227/40484/1d/download.ebz.epson.net/dsc/f/01/00/01/62/55/2b7dee6496e6fdc44d823bd05aaf99d2ea3a38ee/pips-pxb500-Ubuntu10.04-3.2.0-CG.tgz -O /tmp/pips-pxb500-Ubuntu10.04-3.2.0-CG.tgz
 fi
@@ -15,7 +15,7 @@ fi
 tar xzf /tmp/pips-pxb500-Ubuntu10.04-3.2.0-CG.tgz -C /tmp
 bash /tmp/pips-pxb500-Ubuntu10.04-3.2.0-CG.install --noexec --keep --nox11 --target /tmp/release
 
-# fix pips-common package on Ubuntu Oneiric
+# fix pips-common package
 RELEASE=$(lsb_release -rs | tr -d ".")
 if [ ${RELEASE} -lt 1004 ]; then
   DEBDIR=/tmp/release/deb
@@ -27,12 +27,12 @@ if [ ${RELEASE} -lt 1004 ]; then
   dpkg -b ${DEBDIR}/ /tmp/release/pips-common_3.2.0-3_i386.deb
 fi
 
-# install b500 driver
+# install driver
 sudo dpkg -i --force-architecture /tmp/release/pips-common_3.2.0-3_i386.deb
 sudo dpkg -i --force-architecture /tmp/release/pips-ubuntu10.04_3.2.0-3_i386.deb
 sudo dpkg -i --force-architecture /tmp/release/pips-pxb500_3.2.0-3_i386.deb
  
-# create network printer if IP specified
+# create network printer when IP specified
 if [ -n "${1}" ]; then
     sudo lpadmin -p Epson_B-500DN -v socket://${1}:2501 -E -P /usr/share/cups/model/ekpxb500.ppd
 fi
