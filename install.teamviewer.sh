920393887fad3b42a9ef17c49c7ac0175fe77e47
panticz <mail@konczalski.de>
2013-10-23 11:40:48 +0200

    Fix installation for Ubuntu 13.10

--- install.teamviewer.sh
+++ install.teamviewer.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # install required libs
-sudo apt-get install -y libc6-i386 lib32asound2 lib32z1 ia32-libs
+RELEASE=$(lsb_release -rs | tr -d ".")
+if [ ${RELEASE} -ge 1310 ]; then
+  sudo apt-get install -y libxtst6
+else
+  sudo apt-get install -y libc6-i386 lib32asound2 lib32z1 ia32-libs
+fi
 
-if [ "$(uname -m)" == "x86_64" ]; then
+if [ "$(uname -m)" == "x86_64" -a ${RELEASE} -lt 1310 ]; then
   # 64 bit
   URL=http://download.teamviewer.com/download/teamviewer_linux_x64.deb
 else
