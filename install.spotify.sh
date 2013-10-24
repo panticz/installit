5a6c557cbb6380ef2057608f94f8d86e9b315fde
panticz <mail@konczalski.de>
2013-10-24 11:36:17 +0200

    perform quite update

--- install.spotify.sh
+++ install.spotify.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 
 # update
-sudo apt-get update
+sudo apt-get update -qq
 
 # install spotify
 sudo apt-get install -y spotify-client
