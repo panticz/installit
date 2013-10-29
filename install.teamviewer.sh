fe7d70c9b9a6d1a41ea9e3515654d92173eb6aa5
panticz <mail@konczalski.de>
2013-10-29 14:56:15 +0100

    Fix missing i386 libraries

--- install.teamviewer.sh
+++ install.teamviewer.sh
@@ -4,6 +4,14 @@
 RELEASE=$(lsb_release -rs | tr -d ".")
 if [ ${RELEASE} -ge 1310 ]; then
   sudo apt-get install -y libxtst6:i386
+  sudo apt-get install -y gcc-4.8-base:i386
+  sudo apt-get install -y libc6:i386
+  sudo apt-get install -y libgcc1:i386
+  sudo apt-get install -y libx11-6:i386
+  sudo apt-get install -y libxau6:i386  
+  sudo apt-get install -y libxcb1:i386
+  sudo apt-get install -y libxdmcp6:i386
+  sudo apt-get install -y libxext6:i386 
 else
   sudo apt-get install -y libc6-i386 lib32asound2 lib32z1 ia32-libs
 fi
