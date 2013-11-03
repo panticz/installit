eb083340427daac85af2224d251224dc6ad62b1b
panticz <mail@konczalski.de>
2013-11-03 19:38:24 +0100

    Create install.pipelight.sh

---
+++ install.pipelight.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# add repository
+sudo apt-add-repository -y ppa:ehoover/compholio
+sudo apt-add-repository -y ppa:mqchael/pipelight
+
+# update repository
+sudo apt-get update
+
+# install pipelight
+sudo apt-get install pipelight
