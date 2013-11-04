9a40b462bec1f5cb0fba01a7910a91184a175fce
panticz <mail@konczalski.de>
2013-11-04 14:26:59 +0100

    Update install.vlc.sh

--- install.vlc.sh
+++ install.vlc.sh
@@ -3,6 +3,7 @@
 sudo apt-get install -y vlc
 sudo apt-get install -y vlc-plugin-pulse
 sudo apt-get install -y mozilla-plugin-vlc
+sudo apt-get install -y browser-plugin-vlc
 
 # set vnc as default media player
 sudo sed -i 's|totem.desktop|vlc.desktop|g' /etc/gnome/defaults.list
