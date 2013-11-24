9c366cdc7205850a89d053cce178a7ae889447fa
panticz <mail@konczalski.de>
2013-11-24 23:05:18 +0100

    Create install.lm-sensors.sh

---
+++ install.lm-sensors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ensure that this script is run by root
+if [ $(id -u) -ne 0 ]; then
+  sudo $0
+  exit
+fi
+
+# install lm-sensors
+apt-get install -y lm-sensors
+
+# install the latest version
+if [ "$1" == "-n" ]; then
+  wget http://mirrors.kernel.org/ubuntu/pool/universe/l/lm-sensors/lm-sensors_3.3.3-1ubuntu1_amd64.deb -P /tmp/
+  dpkg -i /tmp/lm-sensors_*.deb
+  rm /tmp/lm-sensors_*.deb
+fi
