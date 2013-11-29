7ef7364019da167438205ea8994218ef5420b9b4
panticz <mail@konczalski.de>
2013-11-29 22:47:25 +0100

    Remove sudo

--- install.dovecot.sh
+++ install.dovecot.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# ensure that this script is run by root
+if [ $(id -u) -ne 0 ]; then
+  sudo $0
+  exit
+fi
+
 # install dovecot
-sudo apt-get install -y dovecot-imapd
+apt-get install -y dovecot-imapd
 
 # enable secure imap only (Port 993)
-sudo sed -i 's|^protocols = imap imaps|protocols = imaps|g' /etc/dovecot/dovecot.conf
+sed -i 's|^protocols = imap imaps|protocols = imaps|g' /etc/dovecot/dovecot.conf
 
 # configure ssl
-sudo sed -i 's|^#ssl_cert_file|ssl_cert_file|g' /etc/dovecot/dovecot.conf
-sudo sed -i 's|^#ssl_key_file|ssl_key_file|g' /etc/dovecot/dovecot.conf
-sudo sed -i 's|^#disable_plaintext_auth = yes|disable_plaintext_auth = yes|g' /etc/dovecot/dovecot.conf
+sed -i 's|^#ssl_cert_file|ssl_cert_file|g' /etc/dovecot/dovecot.conf
+sed -i 's|^#ssl_key_file|ssl_key_file|g' /etc/dovecot/dovecot.conf
+sed -i 's|^#disable_plaintext_auth = yes|disable_plaintext_auth = yes|g' /etc/dovecot/dovecot.conf
 
 # restart
-sudo /etc/init.d/dovecot restart
+/etc/init.d/dovecot restart
 
 # configure mailbox path
-# sudo mail_location = maildir:~/Maildir
+# mail_location = maildir:~/Maildir
