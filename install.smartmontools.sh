ce92e25d58833f590d033bb2ada6b56d3db89e70
panticz <mail@konczalski.de>
2014-01-05 16:18:36 +0100

    ensure that this script is run by root

--- install.smartmontools.sh
+++ install.smartmontools.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install -y smartmontools --no-install-recommends
+# ensure that this script is run by root
+if [ $(id -u) -ne 0 ]; then
+sudo $0
+  exit
+fi
+
+apt-get install -y smartmontools --no-install-recommends
