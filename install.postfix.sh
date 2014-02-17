9898aabf8feacb6b834bfcf90c980ee6f0cdadbc
panticz <mail@konczalski.de>
2014-02-18 00:37:01 +0100

    add postfix installation

---
+++ install.postfix.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ensure that this script is run by root
+if [ $(id -u) -ne 0 ]; then
+sudo $0
+  exit
+fi
+
+# pre-configure
+debconf-set-selections <<\EOF
+postfix postfix/mailname string mx
+postfix postfix/main_mailer_type select Internet Site
+EOF
+ 
+# install
+apt-get install -y postfix
+ 
+# post-configure
+cp /etc/postfix/main.cf  /etc/postfix/main.cf.$(date -I)
+postconf -e 'myhostname = YOUR_DOMAIN.com'
+postconf -e 'relayhost = smtp.YOUR_PROVIDER.com'
+postconf -e 'smtp_sasl_security_options = noanonymous'
+postconf -e 'smtp_sasl_password_maps = hash:/etc/postfix/sasl_password'
+postconf -e 'smtp_sasl_auth_enable = yes'
+postconf -e 'message_size_limit = 0'
+ 
+# configure smtp
+#cp /tmp/etc/postfix/sasl_password /etc/postfix/
+echo 'smtp.example.com your@example.com:password' > /etc/postfix/sasl_password
+postmap /etc/postfix/sasl_password
+ 
+# restart
+/etc/init.d/postfix restart
