f451c43e7b76231b1a795c7f3ece5742eac62da0
panticz <mail@konczalski.de>
2014-02-18 00:34:37 +0100

    Update SSL configuration

--- install.dovecot.sh
+++ install.dovecot.sh
@@ -16,8 +16,7 @@ echo "protocols = imaps" > /usr/share/dovecot/protocols.d/imapd.protocol
 sed -i 's|mbox:~/mail:INBOX=/var/mail/%u|maildir:~/Maildir|g' /etc/dovecot/conf.d/10-mail.conf
 
 # configure ssl
-sed -i 's|^#ssl_cert_file|ssl_cert_file|g' /etc/dovecot/dovecot.conf
-sed -i 's|^#ssl_key_file|ssl_key_file|g' /etc/dovecot/dovecot.conf
+sed -i 's|^#ssl = yes|ssl = yes|g' /etc/dovecot/conf.d/10-ssl.conf
 sed -i 's|^#disable_plaintext_auth = yes|disable_plaintext_auth = yes|g' /etc/dovecot/conf.d/10-auth.conf
 
 # restart
