ecc2bd04e38e544271fbca5cfb0b815ef1e08676
panticz <mail@konczalski.de>
2014-05-08 17:14:01 +0200

    rewrite apt proxy workaround

--- install.java-jdk.sh
+++ install.java-jdk.sh
@@ -21,17 +21,13 @@ sudo add-apt-repository -y ppa:webupd8team/java
 # update repository
 sudo apt-get update -qq
 
-# WORKAROUND: disable apt proxy
-sudo sed -i 's|Acquire::http::Proxy|#DISABLED#Acquire::http::Proxy|g' /etc/apt/apt.conf
-
 # install
-sudo apt-get -o Acquire::http:Proxy=DIRECT install -y oracle-java8-installer
+export dl_direct=DIRECT
+sudo -E apt-get install -y oracle-java8-installer
+unset dl_direct
 
 # set Java environment variables
 sudo apt-get install -y oracle-java8-set-default
-
-# WORKAROUND: enable apt proxy
-sudo sed -i 's|#DISABLED#Acquire::http::Proxy|Acquire::http::Proxy|g' /etc/apt/apt.conf
 
 # set java 7 as default java version
 sudo update-java-alternatives -s java-8-oracle
