81b83c17bbdcab06d4311a0d5eeedbf2a9d87600
panticz <mail@konczalski.de>
2014-05-09 13:39:18 +0200

    add support for new puppet v3

--- install.puppet-client.sh
+++ install.puppet-client.sh
@@ -10,4 +10,11 @@ fi
 apt-get install -qq -y puppet
  
 # configure to autostart puppet on boot
-[ -f /etc/default/puppet ] && sed -i 's|START=no|START=yes|g' /etc/default/puppet
+if [ $(puppet --version | cut -d "." -f1) -ge 3 ]; then
+  # puppet 3.x
+  /etc/init.d/puppet stop
+  puppet agent --enable
+else
+  # puppet 2.x
+  [ -f /etc/default/puppet ] && sed -i 's|START=no|START=yes|g' /etc/default/puppet
+fi
