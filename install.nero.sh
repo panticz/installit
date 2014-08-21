a9ccb9c13db6027dd1107e2c45580ca769309d3c
panticz <mail@konczalski.de>
2014-08-21 13:10:12 +0200

    download NeroLinux package directly from internet

--- install.nero.sh
+++ install.nero.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-URL=$1
-FILE=${URL##*/}
+if [ ! -z $1 ] {
+  URL=$1
+  FILE=${URL##*/}
+} else {
+  # get download URL
+  if [ "$(uname -m)" == "x86_64" ]; then
+    # 64 bit OS found
+    OS="_64"
+  fi
+  URL=$(wget -q http://www.nero.com/deu/downloads/previous-versions/download-linux4-update.php -O - | egrep "^.*dlLink.*http.*x86${OS}.deb" | cut -d'"' -f2)
+
+  # set file name
+  FILE=nerolinux-x86${OS}.deb
+}
 
 # get file
 if [ ${URL:0:4} == "http" ]; then
