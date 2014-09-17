8f16370d5e6d9c203840bd6d9c9d54eb546080ea
panticz <mail@konczalski.de>
2014-09-17 11:38:16 +0200

    auto parse download URL from GitLab page

--- install.gitlab.sh
+++ install.gitlab.sh
@@ -6,8 +6,8 @@ if [ $(id -u) -ne 0 ]; then
     exit
 fi
 
-# download url
-URL=https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.2.0-omnibus-1_amd64.deb
+# parse download URL from GitLab page
+URL=$(wget -q --no-check-certificate https://about.gitlab.com/downloads/archives/ -O - | grep -i href | grep -i $(lsb_release -is)-$(lsb_release -rs) | head -1 | cut -d"\"" -f2)
 
 # download gitlab omnibus package
 wget -q ${URL} -O /tmp/gitlab_amd64.deb
