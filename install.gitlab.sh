83c4224b36d5a3db825cb6cf2f1028eebfa2a8e7
panticz <mail@konczalski.de>
2014-09-17 11:54:26 +0200

    Don’t check the server certificate

--- install.gitlab.sh
+++ install.gitlab.sh
@@ -11,7 +11,7 @@ fi
 URL=$(wget -q --no-check-certificate https://about.gitlab.com/downloads/archives/ -O - | grep -i href | grep -i ${ID}-${VERSION_ID} | head -1 | cut -d"\"" -f2)
 
 # download gitlab omnibus package
-wget -q ${URL} -O /tmp/gitlab_amd64.deb
+wget -q --no-check-certificate ${URL} -O /tmp/gitlab_amd64.deb
 
 # install
 dpkg -i /tmp/gitlab_amd64.deb
