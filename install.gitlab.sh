9e637ca92e4536370b71ba1c2536aaa167fe9927
panticz <mail@konczalski.de>
2014-09-17 11:27:43 +0200

    Update to 7.2.0

--- install.gitlab.sh
+++ install.gitlab.sh
@@ -7,7 +7,7 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 # download url
-URL=https://downloads-packages.s3.amazonaws.com/ubuntu-12.04/gitlab_6.9.2-omnibus-1_amd64.deb
+URL=https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.2.0-omnibus-1_amd64.deb
 
 # download gitlab omnibus package
 wget -q ${URL} -O /tmp/gitlab_amd64.deb
