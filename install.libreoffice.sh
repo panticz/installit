e81a3ab1a37c61ba6efa2f9432f19897b30ca824
panticz <mail@konczalski.de>
2014-10-22 11:28:24 +0200

    remove unnecessary gnome menu entries

--- install.libreoffice.sh
+++ install.libreoffice.sh
@@ -9,5 +9,8 @@ sudo apt-get install -y libreoffice
 sudo apt-get install -y libreoffice-gnome libreoffice-help-de libreoffice-l10n-de libreoffice-officebean libreoffice-java-common libreoffice-base
 sudo apt-get install -y hunspell-de-de hunspell-en-us hyphen-de mythes-de
 
+# remove unnecessary gnome menu entries
+sed -i 's|Education;Science;||' /usr/share/applications/libreoffice-math.desktop
+
 # do we need this?
 #sudo apt-get install -y openoffice.org openoffice.org-style-human openoffice.org-java-common
