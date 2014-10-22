4dce3a5cf09a7eccdb4b7f1e16a08e29aba52e82
panticz <mail@konczalski.de>
2014-10-22 13:46:11 +0200

    deinstall previous twinkle and exclude from update

--- install.twinkle.sh
+++ install.twinkle.sh
@@ -3,9 +3,11 @@
 RELEASE=$(lsb_release -rs | tr -d ".")
 if [ ${RELEASE} -ge 1404 ]; then
   # use precise, trusty seams to by broken
+  apt-get remove -y twinkle
   echo "deb http://ubuntu.mirror.cambrium.nl/ubuntu/ precise main universe" > /etc/apt/sources.list.d/twinkle.list
   apt-get update
   apt-get install twinkle/precise
+  echo "twinkle hold" | sudo dpkg --set-selections
   rm /etc/apt/sources.list.d/twinkle.list
   apt-get update
 else
