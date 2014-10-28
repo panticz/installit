cc79a3377360184b4491f26e4bd4d60906ff0250
panticz <mail@konczalski.de>
2014-10-28 15:51:32 +0100

    try to fix possible dependency errors

--- install.mysql-admin.sh
+++ install.mysql-admin.sh
@@ -7,3 +7,6 @@ sudo dpkg -i /tmp/libcairo2_1.13.0~20140204-0ubuntu1_amd64.deb
 # download and install mysql-workbench
 wget http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.2.3-1ubu1404-amd64.deb -P /tmp
 sudo dpkg -i /tmp/mysql-workbench-community-6.2.3-1ubu1404-amd64.deb
+
+# fix possible dependency errors
+apt-get install -f
