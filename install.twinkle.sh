937b167777c9cc1a0be5460547463eef9b53adaf
panticz <mail@konczalski.de>
2014-10-28 14:41:04 +0100

    autoinstall

--- install.twinkle.sh
+++ install.twinkle.sh
@@ -6,7 +6,7 @@ if [ ${RELEASE} -ge 1404 ]; then
   apt-get remove -y twinkle
   echo "deb http://ubuntu.mirror.cambrium.nl/ubuntu/ precise main universe" > /etc/apt/sources.list.d/twinkle.list
   apt-get update
-  apt-get install twinkle/precise
+  apt-get install -y twinkle/precise
   echo "twinkle hold" | sudo dpkg --set-selections
   rm /etc/apt/sources.list.d/twinkle.list
   apt-get update
