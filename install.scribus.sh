6d0c8b5836e90792d29c2b7253c4d762c3334e8c
panticz <mail@konczalski.de>
2014-11-04 00:44:37 +0100

    rewrite repository

--- install.scribus.sh
+++ install.scribus.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-sudo apt-add-repository -y http://debian.scribus.net/debian
+cat <<EOF> /etc/apt/sources.list.d/scribus.list
+deb http://debian.scribus.net/debian/ raring main
+deb http://debian.tagancha.org/debian/ raring main
+EOF
 
 sudo gpg --keyserver wwwkeys.pgp.net --recv-keys EEF818CF
 sudo gpg --armor --export EEF818CF | sudo apt-key add -
