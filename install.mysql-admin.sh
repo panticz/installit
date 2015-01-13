0d35b48612010df5716b9722259d7132532472d1
panticz <mail@konczalski.de>
2015-01-13 14:20:07 +0100

    update to 6.2.4

--- install.mysql-admin.sh
+++ install.mysql-admin.sh
@@ -5,8 +5,8 @@ wget http://mirrors.kernel.org/ubuntu/pool/main/c/cairo/libcairo2_1.13.0~2014020
 sudo dpkg -i /tmp/libcairo2_1.13.0~20140204-0ubuntu1_amd64.deb
 
 # download and install mysql-workbench
-wget http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.2.3-1ubu1404-amd64.deb -P /tmp
-sudo dpkg -i /tmp/mysql-workbench-community-6.2.3-1ubu1404-amd64.deb
+wget http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.2.4-1ubu1404-amd64.deb -P /tmp
+sudo dpkg -i /tmp/mysql-workbench-community-*-1ubu1404-amd64.deb
 
 # fix possible dependency errors
 apt-get install -f
