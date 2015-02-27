52500c32179949f2606fc6229dd76b5b584191f4
panticz <mail@konczalski.de>
2015-02-27 14:50:31 +0100

    add installer script for Google music manager

---
+++ install.google-musicmanager.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# get URL
+if [ "$(uname -m)" == "x86_64" ]; then
+  # 64 bit
+  URL=https://dl.google.com/linux/direct/google-musicmanager-beta_current_amd64.deb
+else
+  # 32 bit
+  URL=https://dl.google.com/linux/direct/google-musicmanager-beta_current_i386.deb
+fi
+
+# download
+wget ${URL} -P /tmp
+
+# install
+sudo dpkg -i /tmp/google-musicmanager-beta_current_amd64.deb
