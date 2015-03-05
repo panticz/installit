a0e32ad684badbbcedd6aed874389dde12f59a59
panticz <mail@konczalski.de>
2015-03-05 16:21:42 +0100

    update libcairo2

--- install.mysql-workbench.sh
+++ install.mysql-workbench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install libcairo2
-wget http://mirrors.kernel.org/ubuntu/pool/main/c/cairo/libcairo2_1.13.0~20140204-0ubuntu1_amd64.deb -P /tmp
-sudo dpkg -i /tmp/libcairo2_1.13.0~20140204-0ubuntu1_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/main/c/cairo/libcairo2_1.13.0~20140204-0ubuntu1.1_amd64.deb -P /tmp
+sudo dpkg -i /tmp/libcairo2_*_amd64.deb
 
 # download and install mysql-workbench
 wget http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.2.4-1ubu1404-amd64.deb -P /tmp
