b3ec9d81aa69a2420f463032a53f666b1f754aae
panticz <mail@konczalski.de>
2015-04-07 09:58:02 +0200

    save witch version number

--- install.gitlab.sh
+++ install.gitlab.sh
@@ -11,10 +11,10 @@ fi
 URL=$(wget -q --no-check-certificate https://about.gitlab.com/downloads/archives/ -O - | grep -i href | grep -i ${ID}-${VERSION_ID} | head -1 | cut -d"\"" -f2)
 
 # download gitlab omnibus package
-wget -q --no-check-certificate ${URL} -O /tmp/gitlab_amd64.deb
+wget -q --no-check-certificate ${URL} -P /tmp/${URL##*/}
 
 # install
-dpkg -i /tmp/gitlab_amd64.deb
+dpkg -i /tmp/gitlab_*_amd64.deb
 
 # configure
 gitlab-ctl reconfigure
