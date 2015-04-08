427636dbd0c602c68f2631a676a29f4c6c4da2e7
panticz <mail@konczalski.de>
2015-04-08 08:48:46 +0200

    add script to install and configure Thinkpad fan

---
+++ hardware/install.thinkfan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# install
+sudo apt-get install -y thinkfan
+
+# configure
+sudo sed -i 's|START=no|START=yes|' /etc/default/thinkfan
+sudo sed -i 's|DAEMON_ARGS="-q"|DAEMON_ARGS="-q -b 1 -s 15"|' /etc/default/thinkfan
+
+cat <<EOF> /etc/thinkfan.conf
+hwmon /sys/devices/virtual/hwmon/hwmon0/temp1_input
+(0,	0,	65)
+(1,	65,	70)
+(2,	70,	75)
+(3,	75,	80)
+(4,	80,	85)
+(5,	85,	90)
+(7,	90,	32767)
+EOF
