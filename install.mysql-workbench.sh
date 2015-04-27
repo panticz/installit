bc7fd459f708e882377a6ca6879491a18cf04c05
panticz <mail@konczalski.de>
2015-04-27 20:42:57 +0200

    Update MySQL Workbench to v6.3.3

--- install.mysql-workbench.sh
+++ install.mysql-workbench.sh
@@ -5,7 +5,7 @@ wget http://mirrors.kernel.org/ubuntu/pool/main/c/cairo/libcairo2_1.13.0~2014020
 sudo dpkg -i /tmp/libcairo2_*_amd64.deb
 
 # download and install mysql-workbench
-wget http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.2.4-1ubu1404-amd64.deb -P /tmp
+wget http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.3.3-1ubu1410-amd64.deb -P /tmp
 sudo dpkg -i /tmp/mysql-workbench-community-*-1ubu1404-amd64.deb
 
 # fix possible dependency errors
