a9deff361922cb91f9401c84da94a500af33f341
panticz <mail@konczalski.de>
2015-04-30 18:58:27 +0200

    switch to GitLab Community Edition

--- install.gitlab.sh
+++ install.gitlab.sh
@@ -6,18 +6,14 @@ if [ $(id -u) -ne 0 ]; then
     exit
 fi
 
-# parse download URL from GitLab page
-. /etc/os-release 
-URL=$(wget -q --no-check-certificate https://about.gitlab.com/downloads/archives/ -O - | grep -i href | grep -i ${ID}-${VERSION_ID} | head -1 | cut -d"\"" -f2)
+# install required packages
+apt-get install -y ca-certificates curl
 
-# download gitlab omnibus package
-wget -q --no-check-certificate ${URL} -P /tmp/${URL##*/}
+# download GitLab installation script
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb | bash
 
-# install
-dpkg -i /tmp/gitlab_*_amd64.deb
-
-# configure
-gitlab-ctl reconfigure
+# install GitLab Community Edition
+apt-get install -y gitlab-ce
 
 # configure domain
 sed -i "s|gitlab.example.com|$($hostname -A)|g" /var/opt/gitlab/gitlab-rails/etc/gitlab.yml
