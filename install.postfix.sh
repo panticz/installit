1a3e83e6b3fce4512da5908bfbe3e9f0b7fcf0ad
panticz <mail@konczalski.de>
2015-05-11 17:35:51 +0200

    add user mapping configuration

--- install.postfix.sh
+++ install.postfix.sh
@@ -18,16 +18,20 @@ apt-get install -y postfix
 # post-configure
 cp /etc/postfix/main.cf  /etc/postfix/main.cf.$(date -I)
 postconf -e 'myhostname = example.com'
-postconf -e 'relayhost = smtp.gmail.com'
 postconf -e 'smtp_sasl_security_options = noanonymous'
-postconf -e 'smtp_sasl_password_maps = hash:/etc/postfix/sasl_password'
 postconf -e 'smtp_sasl_auth_enable = yes'
 postconf -e 'message_size_limit = 0'
  
 # configure smtp
-#cp /tmp/etc/postfix/sasl_password /etc/postfix/
 echo 'smtp.gmail.com you@gmail.com:password' > /etc/postfix/sasl_password
 postmap /etc/postfix/sasl_password
- 
+postconf -e 'relayhost = smtp.gmail.com'
+postconf -e 'smtp_sasl_password_maps = hash:/etc/postfix/sasl_password'
+
+# configure user mapping
+echo "root@postfix.example.com you@gmail.com" >> /etc/postfix/generic
+postmap /etc/postfix/generic
+postconf -e 'smtp_generic_maps = hash:/etc/postfix/generic'
+
 # restart
 /etc/init.d/postfix restart
