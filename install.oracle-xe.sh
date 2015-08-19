e1918530ceb01fc1d260b3404ea93f94da99308e
panticz <mail@konczalski.de>
2015-08-20 01:10:45 +0200

    use https to access repository

--- install.oracle-xe.sh
+++ install.oracle-xe.sh
@@ -2,7 +2,7 @@
 
 # add oracle repository
 wget http://oss.oracle.com/el4/RPM-GPG-KEY-oracle -O- | apt-key add -
-echo "deb http://oss.oracle.com/debian unstable main non-free" > /etc/apt/sources.list.d/oracle.list
+echo "deb https://oss.oracle.com/debian unstable main non-free" > /etc/apt/sources.list.d/oracle.list
  
 # update repository
 apt-get update
