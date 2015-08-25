8e4fd4f1f04e422bb42b11aec306c31bef8fb077
panticz <mail@konczalski.de>
2015-08-25 23:38:53 +0200

    use Ubunut Trusty repository on Utopic

--- install.lxd.sh
+++ install.lxd.sh
@@ -3,5 +3,8 @@
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
 
+# use Ubunut Trusty repository on Utopic
+sed -i 's|utopic|trusty|g' /etc/apt/sources.list.d/ubuntu-lxc-ubuntu-lxd-stable-utopic.list
+
 sudo apt-get update
 sudo apt-get install -y lxd
