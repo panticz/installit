d149dcf7808cc931879dd39c16ba8f73a433a974
panticz <mail@konczalski.de>
2015-09-02 22:45:22 +0200

    install bc

--- install.oracle-xe.sh
+++ install.oracle-xe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install required applications
-apt-get install -y wget apt-transport-https
+apt-get install -y wget apt-transport-https bc
 
 # add oracle repository
 wget http://oss.oracle.com/el4/RPM-GPG-KEY-oracle -O- | apt-key add -
