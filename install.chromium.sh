e7ccceec3aaf3d7da652aa67a66699bb18ac5d4d
panticz <mail@konczalski.de>
2016-04-01 11:31:22 +0200

    fix not yet availlable Ubuntu Xenial repository

--- install.chromium.sh
+++ install.chromium.sh
@@ -2,6 +2,9 @@
 
 sudo apt-add-repository -y ppa:chromium-daily/stable
 
+# fix not yet availlable Ubuntu Xenial repository
+[ -f /etc/apt/sources.list.d/chromium-daily-ubuntu-stable-xenial.list ] && sed -i 's|xenial|devel|' /etc/apt/sources.list.d/chromium-daily-ubuntu-stable-xenial.list
+
 sudo apt-get update -y -qq
 
 sudo apt-get install -y chromium-browser
