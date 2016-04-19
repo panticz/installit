a7e00b1a81f62f263e162ef92b9e82db4efbc409
panticz <mail@konczalski.de>
2016-04-19 21:48:46 +0200

    Update to Tomcat 8

--- install.tomcat.sh
+++ install.tomcat.sh
@@ -10,18 +10,10 @@ fi
 wget -q https://raw.githubusercontent.com/panticz/installit/master/install.java-jdk.sh -O - | bash -
 
 # install tomcat
-apt-get install -y tomcat7
+apt-get install -y tomcat8
 
 # configure JAVA_HOME
 echo "JAVA_HOME=/usr/lib/jvm/$(ls /usr/lib/jvm | grep oracle | tail -1)" >> /etc/default/tomcat?
-
-# install authbind
-apt-get install -y authbind
-
-# create /etc/authbind/byport/80
-touch /etc/authbind/byport/80
-chmod 500 /etc/authbind/byport/80
-chown tomcat7:tomcat7 /etc/authbind/byport/80
 
 # configure tomcat to port 80
 sed -i 's|port="8080"|port="80"|g' /etc/tomcat?/server.xml
@@ -30,4 +22,4 @@ sed -i 's|port="8080"|port="80"|g' /etc/tomcat?/server.xml
 sed -i 's|#AUTHBIND=no|AUTHBIND=yes|g' /etc/default/tomcat?
 
 # restart tomcat
-/etc/init.d/tomcat7 restart
+service tomcat8 restart
