69c8c6570631852e0fa9e570c1e050dd7b7b84bf
panticz <mail@konczalski.de>
2016-05-03 18:42:55 +0200

    Add script to install TeamViewer QS

---
+++ install.teamviewer_qs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget -q http://download.teamviewer.com/download/teamviewer_qs.tar.gz -O /tmp/teamviewer_qs.tar.gz
+sudo tar -C /opt/ -xzf /tmp/teamviewer_qs.tar.gz
+
+cat << EOF | sudo tee -a "/usr/share/applications/teamviewerqs.desktop"
+[Desktop Entry]
+Version=1.0
+Encoding=UTF-8
+Name=TeamViewer QS
+Exec=/opt/teamviewerqs/teamviewer
+Icon=/opt/teamviewer/tv_bin/desktop/teamviewer.png
+Type=Application
+Categories=Network;
+EOF
