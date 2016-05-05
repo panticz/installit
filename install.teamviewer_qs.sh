1e910749827fda71f2942cbff69df6532c9bb1b7
panticz <mail@konczalski.de>
2016-05-05 16:20:59 +0200

    Download TeamViewer QS on-the-fly

--- install.teamviewer_qs.sh
+++ install.teamviewer_qs.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-wget -q http://download.teamviewer.com/download/teamviewer_qs.tar.gz -O /tmp/teamviewer_qs.tar.gz
-sudo tar -C /opt/ -xzf /tmp/teamviewer_qs.tar.gz
+# create teamviewerqs starter
+cat << EOF | sudo tee -a /usr/sbin/teamviewerqs
+#!/bin/bash
 
+wget -q http://download.teamviewer.com/download/teamviewer_qs.tar.gz -O - | tar -C /tmp/ -xz
+/tmp/teamviewerqs/teamviewer
+EOF
+sudo chmod a+x /usr/sbin/teamviewerqs
+
+# download icon
+wget -q http://www.icons101.com/icon_svg/id_54184/teamviewer.svg -O /usr/share/icons/teamviewer.svg
+
+# create starter
 cat << EOF | sudo tee -a "/usr/share/applications/teamviewerqs.desktop"
 [Desktop Entry]
 Version=1.0
 Encoding=UTF-8
 Name=TeamViewer QS
-Exec=/opt/teamviewerqs/teamviewer
-Icon=/opt/teamviewerqs/tv_bin/desktop/teamviewer.png
+Exec=/usr/sbin/teamviewerqs
+Icon=/usr/share/icons/teamviewer.svg
 Type=Application
 Categories=Network;
 EOF
