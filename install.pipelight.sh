623f34ab21825778add280b30e960c6b7c477371
panticz <mail@konczalski.de>
2016-06-12 19:19:34 +0200

    switch to pipelight PPA repository

--- install.pipelight.sh
+++ install.pipelight.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # add repository
-sudo apt-add-repository -y ppa:ehoover/compholio
-sudo apt-add-repository -y ppa:mqchael/pipelight
+sudo add-apt-repository -y ppa:pipelight/stable
 
 # update repository
 sudo apt-get update
 
 # install pipelight
-sudo apt-get install -y pipelight
+sudo apt-get install -y pipelight-multi
+
+sudo pipelight-plugin --update
+sudo pipelight-plugin --accept --enable silverlight
+sudo pipelight-plugin --accept --enable flash
+sudo pipelight-plugin --create-mozilla-plugins
