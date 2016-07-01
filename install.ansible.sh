10ff074e9aec9a5c75e494390ef86d84b2ab0dd2
panticz <mail@konczalski.de>
2016-07-01 12:50:21 +0200

    Remove obsolete Ubuntu Xenial repository patch

--- install.ansible.sh
+++ install.ansible.sh
@@ -6,9 +6,6 @@ sudo apt-get install -y software-properties-common
 # add ansible repository
 sudo apt-add-repository -y ppa:ansible/ansible
 
-# fix not yet availlable Ubuntu Xenial repository
-[ -f /etc/apt/sources.list.d/ansible-ubuntu-ansible-xenial.list ] && sed -i 's|xenial|devel|' /etc/apt/sources.list.d/ansible-ubuntu-ansible-xenial.list
-
 # update package list
 sudo apt-get update
 
