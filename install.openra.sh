3ba1b699c57363c23026adbe0d485d2b29e1e482
panticz <mail@konczalski.de>
2017-02-05 15:22:02 +0100

    fix sudo

--- install.openra.sh
+++ install.openra.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-apt-get install libmono-i18n4.0-all
-apt-get install -f
+sudo apt-get install libmono-i18n4.0-all
 wget -q https://github.com/OpenRA/OpenRA/releases/download/release-20161019/openra_release.20161019_all.deb -O /tmp/openra_release_all.deb
 sudo dpkg -i /tmp/openra_release_all.deb
 sudo apt-get install -f
