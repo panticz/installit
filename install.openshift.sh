d32d56a7a2793f85c21f7771ab94a9c6fd7777ac
panticz <mail@konczalski.de>
2017-02-05 21:59:18 +0100

    Add script to instlal OpenShift

---
+++ install.openshift.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install docker-engine git wget
+
+sudo sed -i 's|ExecStart=/usr/bin/dockerd -H fd://|ExecStart=/usr/bin/dockerd -H fd:// --insecure-registry 172.30.0.0/16|g' /etc/systemd/system/multi-user.target.wants/docker.service
+
+systemctl enable docker
+systemctl start docker
+
+wget https://github.com/openshift/origin/releases/download/v1.4.1/openshift-origin-client-tools-v1.4.1-3f9807a-linux-64bit.tar.gz -qO /tmp/openshift-origin-client-tools-linux-64bit.tar.gz 
+tar xzf /tmp/openshift-origin-client-tools-linux-64bit.tar.gz -C /tmp/
+cp /tmp/openshift-origin-client-tools-*/oc /usr/local/bin/
+
+rm -rf /tmp/openshift-origin-client-tools*
