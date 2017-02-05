0a0605645b3af21c0748b06494addb51ff0997a0
panticz <mail@konczalski.de>
2017-02-05 15:12:23 +0100

    Update to 20161019

--- install.openra.sh
+++ install.openra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -q https://github.com/OpenRA/OpenRA/releases/download/release-20160508/openra_release.20160508_all.deb -O /tmp/openra_release_all.deb
+wget -q https://github.com/OpenRA/OpenRA/releases/download/release-20161019/openra_release.20161019_all.deb -O /tmp/openra_release_all.deb
 sudo dpkg -i /tmp/openra_release_all.deb
 sudo apt-get install -f
