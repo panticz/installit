a8ed3bb29ba711eff8f3f521ea499d9a54caf40c
panticz <mail@konczalski.de>
2017-02-05 15:21:22 +0100

    Install required libs

--- install.openra.sh
+++ install.openra.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get install libmono-i18n4.0-all
+apt-get install -f
 wget -q https://github.com/OpenRA/OpenRA/releases/download/release-20161019/openra_release.20161019_all.deb -O /tmp/openra_release_all.deb
 sudo dpkg -i /tmp/openra_release_all.deb
 sudo apt-get install -f
