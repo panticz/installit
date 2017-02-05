6a6ef74a816a5473e9804c338e4749cd1f120e76
panticz <mail@konczalski.de>
2017-02-05 21:44:53 +0100

    Use Docker repository

--- install.docker.sh
+++ install.docker.sh
@@ -6,4 +6,10 @@ if [ $(id -u) -ne 0 ]; then
   exit
 fi
 
-apt-get install docker.io
+apt-get install -y curl linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates software-properties-common
+
+curl -fsSL https://yum.dockerproject.org/gpg | apt-key add -
+add-apt-repository deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+apt-get update
+
+apt-get -y install docker-engine
