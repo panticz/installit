f69e009361d6aa76ab979f1fa636246781f3aa28
panticz <mail@konczalski.de>
2017-10-11 12:13:29 +0200

    Generate less information

--- install.wine.sh
+++ install.wine.sh
@@ -4,7 +4,7 @@
 sudo dpkg --add-architecture i386
 wget -q https://dl.winehq.org/wine-builds/Release.key -O - | sudo apt-key add -
 sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
-sudo apt-get update
+sudo apt-get update -q
 
 # pre-answer installation questions
 sudo debconf-set-selections <<\EOF
