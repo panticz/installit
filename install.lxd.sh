1099a802ea410ff4382f2f8626f61d55e996bec1
panticz <mail@konczalski.de>
2018-07-01 14:37:15 +0200

    Add LXD PPA only before Ubuntu 18.04

--- install.lxd.sh
+++ install.lxd.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# add LXD PPA repository
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
-sudo apt-get update
+RELEASE=$(lsb_release -rs | tr -d ".")
+if [ ${RELEASE} -lt 1804 ]; then
+    # add LXD PPA repository
+    sudo apt-get install -y software-properties-common
+    sudo add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
+    sudo apt-get update
+fi
 
 # install LXD
 sudo apt-get install -y lxd
