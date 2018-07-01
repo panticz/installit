80501433e7be1e5c942262b95e5935d571c3abe0
panticz <mail@konczalski.de>
2018-07-01 15:20:25 +0200

    Add support for 64 TeamViewer

--- install.teamviewer.sh
+++ install.teamviewer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-URL=http://download.teamviewer.com/download/teamviewer_i386.deb
-
-# add required 32 bit architecture on a 64 bit system
-sudo dpkg --add-architecture i386
+URL=http://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 
 # install required applications
-sudo apt-get install -y wget
+sudo apt-get install -y wget libqt5qml5 libqt5quick5 libqt5webkit5
 
 # download teamviewer
 wget -q ${URL} -O /tmp/teamviewer.deb
+
+# fix broken dependencies
+sudo apt install -y -f
 
 # install teamviewer
 sudo apt install -y /tmp/teamviewer.deb
