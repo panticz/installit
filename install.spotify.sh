c664aaafe471d41dec25e32ddffbca510607a7eb
panticz <mail@konczalski.de>
2018-09-15 19:48:07 +0200

    Update install.spotify.sh

--- install.spotify.sh
+++ install.spotify.sh
@@ -4,7 +4,7 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 
 # add spotify repository
-sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
 # update repository list
 sudo apt-get update -qq
