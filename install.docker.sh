4e57d0a1316d340008c48f121a03444d40c087ab
panticz <mail@konczalski.de>
2018-10-14 20:17:33 +0200

    update package list

--- install.docker.sh
+++ install.docker.sh
@@ -9,5 +9,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # add Docker repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
+# update package list
+sudo apt-get update
+
 # install Docker
 sudo apt-get install -y docker-ce
