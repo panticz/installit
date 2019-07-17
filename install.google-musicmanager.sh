f936d3d5e959bc71cdca3242e65dccefb13e9caa
panticz <mail@konczalski.de>
2019-07-17 20:43:39 +0200

    Remove 32 bit version

--- install.google-musicmanager.sh
+++ install.google-musicmanager.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 # get URL
-if [ "$(uname -m)" == "x86_64" ]; then
-  # 64 bit
-  URL=https://dl.google.com/linux/direct/google-musicmanager-beta_current_amd64.deb
-else
-  # 32 bit
-  URL=https://dl.google.com/linux/direct/google-musicmanager-beta_current_i386.deb
-fi
+URL=https://dl.google.com/linux/direct/google-musicmanager-beta_current_amd64.deb
 
 # download
 wget ${URL} -P /tmp
