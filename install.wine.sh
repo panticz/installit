4ad17f932d307a7a7e57886713680e3c7a383a8d
panticz <mail@konczalski.de>
2019-09-20 10:53:28 +0200

    Uncomment deprecated / unnecessary commands

--- install.wine.sh
+++ install.wine.sh
@@ -7,21 +7,21 @@ wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
 
 # pre-answer installation questions
-sudo debconf-set-selections <<\EOF
-msttcorefonts msttcorefonts/defoma note
-ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula boolean true
-ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note
-EOF
+#sudo debconf-set-selections <<\EOF
+#msttcorefonts msttcorefonts/defoma note
+#ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula boolean true
+#ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note
+#EOF
 
 # install wine
-if [ "$1" == "-s" ]; then
-  # staging release
-  sudo apt-get install -y --install-recommends winehq-staging
-else
-  # stable release
+#if [ "$1" == "-s" ]; then
+#  # staging release
+#  sudo apt-get install -y --install-recommends winehq-staging
+#else
+#  # stable release
   sudo apt-get install -y --install-recommends winehq-stable
-fi
+#fi
 
 # install latest winetricks
 sudo apt-get install -y winetricks
-sudo winetricks --self-update
+#sudo winetricks --self-update
